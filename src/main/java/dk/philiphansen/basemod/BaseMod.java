cd94dde392c0c1b2fec816b1236fad83600f94c1
Philip Hansen <me@philiphansen.dk>
2014-11-25 18:48:07 +0100

    Included all base mod files
    Included all the files required to build the gradle project, and the
    base mod itself. Only thing left is the use documentation.

---
+++ src/main/java/dk/philiphansen/basemod/BaseMod.java
@@ -0,0 +1,22 @@
+package dk.philiphansen.basemod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = "basemod", name = "BaseMod", version = "${version}")
+public class BaseMod {
+	private static Logger logger;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		logger = event.getModLog();
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		logger.info("BaseMod has loaded!");
+	}
+}
